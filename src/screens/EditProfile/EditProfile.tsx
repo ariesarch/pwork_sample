9ae0619783b948b5c5ea64321f5105f3e2ef2926
sevthedev113 <severus.dev113@gmail.com>
2024-11-27 07:10:04 +0630

    Fix: add keyboard aware scroll view

--- src/screens/EditProfile/EditProfile.tsx
+++ src/screens/EditProfile/EditProfile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Pressable, View } from 'react-native';
+import { ActivityIndicator, Pressable, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { useAuthStore } from '@/store/auth/authStore';
 import { cn } from '@/util/helper/twutil';
@@ -20,13 +20,10 @@ import ThemeModal from '@/components/atoms/common/Modal/Modal';
 import { useProfileMediaStore } from '@/store/profile/useProfileMediaStore';
 import ManageAttachmentModal from '@/components/organisms/profile/ManageAttachment/MakeAttachmentModal';
 import { ComposeCameraIcon } from '@/util/svg/icon.compose';
-import { Flow } from 'react-native-animated-spinkit';
-import customColor from '@/util/constant/color';
-import {
-	KeyboardAvoidingView,
-	KeyboardAwareScrollView,
-} from 'react-native-keyboard-controller';
 import { cleanText } from '@/util/helper/cleanText';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import LoadingModal from '@/components/atoms/common/LoadingModal/LoadingModal';
+import Toast from 'react-native-toast-message';
 
 type ProfileType = {
 	display_name?: string;
@@ -93,21 +90,22 @@ const EditProfile = () => {
 		<SafeScreen>
 			<Header
 				hideUnderline
-				title="Edit Profile"
-				leftCustomComponent={<BackButton />}
+				title="Edit Your Profile"
+				leftCustomComponent={
+					<BackButton
+						customOnPress={() => {
+							actions.onSelectMedia('header', []);
+							actions.onSelectMedia('avatar', []);
+							navigation.goBack();
+						}}
+					/>
+				}
 			/>
-			{isPending ? (
-				<View
-					style={{
-						backgroundColor: customColor['patchwork-dark-100'],
-						flex: 1,
-						alignItems: 'center',
-						justifyContent: 'center',
-					}}
-				>
-					<Flow size={50} color={customColor['patchwork-red-50']} />
-				</View>
-			) : (
+			<KeyboardAwareScrollView
+				enableOnAndroid
+				extraScrollHeight={85}
+				contentContainerStyle={{ flexGrow: 1 }}
+			>
 				<View className="flex-1 -mt-2 bg-white dark:bg-patchwork-dark-100">
 					{/* Header Media Modal */}
 					<ThemeModal
@@ -150,7 +148,7 @@ const EditProfile = () => {
 					{/* Header Image */}
 					<Pressable onPress={() => actions.onToggleMediaModal('header')}>
 						<FastImage
-							className="bg-patchwork-dark-50 h-[140]"
+							className="bg-patchwork-dark-50 h-[100]"
 							source={{
 								uri: header.selectedMedia[0]?.uri || profile?.header,
 								priority: FastImage.priority.normal,
@@ -162,34 +160,31 @@ const EditProfile = () => {
 					{/* Avatar Image */}
 					<View className="mx-auto">
 						<Pressable
-							className="p-1 z-10 absolute -bottom-1 -right-1 rounded-full bg-slate-50"
+							className="p-1 "
 							onPress={() => actions.onToggleMediaModal('avatar')}
 						>
-							<ComposeCameraIcon className="" />
+							<View className="z-10 absolute bottom-2 right-2 rounded-full bg-slate-50 p-1">
+								<ComposeCameraIcon className="" />
+							</View>
+							<FastImage
+								className={cn(
+									'w-[100] h-[100] mt-[-25] bg-patchwork-dark-50 border-patchwork-dark-100 border-4 rounded-full',
+								)}
+								source={{
+									uri: avatar.selectedMedia[0]?.uri || profile?.avatar,
+									priority: FastImage.priority.normal,
+								}}
+								resizeMode={FastImage.resizeMode.cover}
+							/>
 						</Pressable>
-						<FastImage
-							className={cn(
-								'w-[70] h-[70] mt-[-25] bg-patchwork-dark-50 border-patchwork-dark-100 border-4 rounded-full',
-							)}
-							source={{
-								uri: avatar.selectedMedia[0]?.uri || profile?.avatar,
-								priority: FastImage.priority.normal,
-							}}
-							resizeMode={FastImage.resizeMode.cover}
-						/>
 					</View>
+					<ThemeText className="mx-auto">{userInfo.display_name}</ThemeText>
 					{/* Profile Form */}
-					<KeyboardAwareScrollView
-						contentContainerStyle={{ flex: 1 }}
-						className="flex-1 mx-3 -mt-5"
-					>
-						<ThemeText variant={'textOrange'} size={'md_16'}>
-							Public Info
-						</ThemeText>
-						<ThemeText className="mt-1" variant={'textOrange'}>
-							Display Name
-						</ThemeText>
+					<View className="mx-5 flex-1">
+						<ThemeText size={'md_16'}>Public Info</ThemeText>
+						<ThemeText className="mt-3 mb-1">Display Name</ThemeText>
 						<TextInput
+							placeholder="Your display name"
 							value={profile?.display_name || ''}
 							onChangeText={name =>
 								setProfile(prev => ({
@@ -198,9 +193,7 @@ const EditProfile = () => {
 								}))
 							}
 						/>
-						<ThemeText className="mt-1" variant={'textOrange'}>
-							Bio
-						</ThemeText>
+						<ThemeText className="mt-2 mb-1">Bio</ThemeText>
 						<TextInput
 							multiline
 							textArea
@@ -213,15 +206,15 @@ const EditProfile = () => {
 							}
 						/>
 						<Button
-							disabled={!profile?.display_name}
 							className="absolute bottom-0 left-0 right-0 mb-5"
 							onPress={handleUpdateProfile}
 						>
 							<ThemeText>Save</ThemeText>
 						</Button>
-					</KeyboardAwareScrollView>
+					</View>
 				</View>
-			)}
+			</KeyboardAwareScrollView>
+			<LoadingModal isVisible={isPending} />
 		</SafeScreen>
 	);
 };
