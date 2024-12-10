14c96f51ac8b3078c58a500a1c22ddd702c84ff9
yemyatthu-binlab <yemyatthu.cs@gmail.com>
2024-12-10 07:11:22 +0630

    Fix::reply query invalidation calling multile time

---
+++ src/util/cache/reply/replyCache.ts
@@ -0,0 +1,39 @@
+import { queryClient } from '@/App';
+import { FeedRepliesQueryKey } from '@/types/queries/feed.type';
+import { DEFAULT_API_URL } from '@/util/constant';
+import { cloneDeep, get, set, times } from 'lodash';
+
+export const updateReplyFeedCache = (
+	feedReplyQueryKey: FeedRepliesQueryKey,
+	newStatus: Pathchwork.Status,
+	feedDetailStatusId: string,
+) => {
+	queryClient.setQueryData<Pathchwork.TimelineReplies>(
+		feedReplyQueryKey,
+		oldData => {
+			if (!oldData) return oldData;
+			console.log('aa::', newStatus.in_reply_to_id, feedDetailStatusId);
+
+			if (newStatus.in_reply_to_id == feedDetailStatusId) {
+				return {
+					...oldData,
+					descendants: [...oldData.descendants, newStatus],
+				};
+			} else {
+				const { domain_name } = feedReplyQueryKey[1];
+				const default_domain = process.env.API_URL ?? DEFAULT_API_URL;
+				if (domain_name == default_domain) {
+					queryClient.invalidateQueries({ queryKey: feedReplyQueryKey });
+					return oldData;
+				}
+				// times(3, i => {
+				// 	setTimeout(() => {
+				// 		console.log('bb::', i);
+				// 		queryClient.invalidateQueries({ queryKey: feedReplyQueryKey });
+				// 	}, i * 800);
+				// });
+				return oldData;
+			}
+		},
+	);
+};
