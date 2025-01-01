131735df657240e884cac7cf88f66d0388ea61e5
Severus <severus.dev113@gmail.com>
2025-01-01 21:26:10 +0630

    pushing original parseHTML file

--- src/components/atoms/common/ParseHtml/ParseHtml.tsx
+++ src/components/atoms/common/ParseHtml/ParseHtml.tsx
@@ -1,33 +1,32 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable default-case */
 
-import { useMemo, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { parseDocument, ElementType } from 'htmlparser2';
 import type { ChildNode } from 'domhandler';
-import { Platform } from 'react-native';
+import { Platform, Pressable, View } from 'react-native';
 import ParseEmojis from '../ParseEmojis/ParnseEmojis';
 import { ThemeText } from '../ThemeText/ThemeText';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { HomeStackParamList } from '@/types/navigation';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useSelectedDomain } from '@/store/feed/activeDomain';
+import { layoutAnimation } from '@/util/helper/timeline';
 import { useAuthStore } from '@/store/auth/authStore';
 
 type Props = {
 	status: Pathchwork.Status;
+	numberOfLines?: number;
 	isMainStatus?: boolean;
-	handleSeeMorePress?: () => void;
-	isFeedDetail?: boolean;
 };
 
-const MAX_CHAR_COUNT = 280;
+const MAX_ALLOWED_LINES = 35;
 
-const HTMLParser = ({
-	status,
-	isMainStatus,
-	handleSeeMorePress,
-	isFeedDetail,
-}: Props) => {
+const HTMLParser = ({ status, numberOfLines = 10, isMainStatus }: Props) => {
+	const [totalLines, setTotalLines] = useState<number>();
+	const [expanded, setExpanded] = useState(false);
+
 	const isFirstLink = useRef(true);
 	const domain_name = useSelectedDomain();
 	const document = useMemo(
@@ -37,7 +36,6 @@ const HTMLParser = ({
 	const adaptedLineheight = Platform.OS === 'ios' ? 18 : undefined;
 	const navigation = useNavigation<StackNavigationProp<HomeStackParamList>>();
 	const { userInfo } = useAuthStore();
-
 	const handleHashTahPress = (tag: string) => {
 		const specialTag = tag.replace(/#/g, '');
 		navigation.navigate('HashTagDetail', {
@@ -50,69 +48,96 @@ const HTMLParser = ({
 		if (mention.id === userInfo?.id!) {
 			navigation.navigate('Profile', { id: userInfo?.id! });
 		} else {
-			navigation.navigate('ProfileOther', { id: mention.id });
+			navigation.navigate('ProfileOther', {
+				id: mention.id,
+			});
 		}
 	};
 
-	// Get unwrapped text content for truncation
-	const unwrappedContent = useMemo(
-		() => document.children.map(unwrapNode).join(''),
-		[document],
-	);
-
-	const isTruncated = unwrappedContent.length > MAX_CHAR_COUNT;
-
-	// Render the parsed nodes with mentions and hashtags
-	const renderContent = () =>
-		document.children.map((node, index) => renderNode(node, index));
-
 	const renderNode = (node: ChildNode, index: number) => {
-		switch (node.type) {
+		let classes;
+		// let href: string;
+		switch (node?.type) {
 			case ElementType.Text:
-				return <ParseEmojis content={node.data.trim()} key={index} />;
+				let content: string = node.data;
+
+				if (node.data.trim().length) {
+					content = node?.data.replace(/^\s+/, '');
+				} else {
+					content = node.data.trim();
+				}
+				return <ParseEmojis content={content} key={index} />;
 
 			case ElementType.Tag:
 				switch (node.name) {
 					case 'a':
-						const classes = node.attribs.class;
-						const children = node.children.map(unwrapNode).join('');
+						classes = node.attribs.class;
+						// href = node.attribs.href;
 
-						if (classes?.includes('hashtag')) {
+						if (classes) {
+							if (classes.includes('hashtag')) {
+								const children = node.children.map(unwrapNode).join('');
+
+								return (
+									<ThemeText
+										key={index}
+										size={'fs_13'}
+										className="font-SourceSans3_SemiBold"
+										children={`${children} `}
+										onPress={() => handleHashTahPress(children)}
+									/>
+								);
+							}
+
+							if (classes.includes('mention') && status?.mentions?.length) {
+								const mentionedText = node.children.map(unwrapNode).join('');
+
+								const matchedMention = (status?.mentions || []).find(
+									(mention: Pathchwork.Mention) =>
+										`@${mention.username}` === mentionedText,
+								);
+
+								return (
+									<ThemeText
+										key={index}
+										variant={matchedMention ? 'textOrange' : 'textGrey'}
+										size={isMainStatus ? 'default' : 'fs_13'}
+										children={`${mentionedText} `}
+										onPress={() => {
+											if (matchedMention) handleMentionPress(matchedMention);
+										}}
+									/>
+								);
+							}
+						}
+
+						if (isImageMissing) {
+							const contentNode = node.children
+								.map(child => unwrapNode(child))
+								.join('');
+
 							return (
 								<ThemeText
 									key={index}
+									variant="textOrange"
 									size="fs_13"
-									className="font-SourceSans3_SemiBold"
-									children={`${children} `}
-									onPress={() => handleHashTahPress(children)}
+									children={`${contentNode} `}
 								/>
 							);
 						}
-
-						if (classes?.includes('mention') && status?.mentions?.length) {
-							const mentionedText = children;
-							const matchedMention = status.mentions.find(
-								mention => `@${mention.username}` === mentionedText,
-							);
-
-							return (
-								<ThemeText
-									key={index}
-									variant={matchedMention ? 'textOrange' : 'textGrey'}
-									size={isMainStatus ? 'default' : 'fs_13'}
-									children={`${mentionedText} `}
-									onPress={() =>
-										matchedMention && handleMentionPress(matchedMention)
-									}
-								/>
-							);
+						const nodeContent = node.children
+							.map(child => unwrapNode(child))
+							.join('');
+						if (isFirstLink && status?.is_meta_preview && status?.card) {
+							isFirstLink.current = false;
+							return null;
 						}
 						return (
 							<ThemeText
 								key={index}
-								variant="textOrange"
 								size="fs_13"
-								children={`${children} `}
+								variant="textOrange"
+								children={nodeContent}
 							/>
 						);
 
@@ -130,11 +155,36 @@ const HTMLParser = ({
 							</ThemeText>
 						);
 
+					case 'p':
+						if (index < document.children.length - 1) {
+							return (
+								<ThemeText key={index}>
+									{node.children.map((c, i) => renderNode(c, i))}
+									<ThemeText
+										style={{
+											lineHeight: adaptedLineheight
+												? adaptedLineheight / 2
+												: undefined,
+										}}
+									>
+										{'\n'}
+									</ThemeText>
+								</ThemeText>
+							);
+						}
+						return (
+							<ThemeText
+								key={index}
+								children={node.children.map((c, i) => renderNode(c, i))}
+							/>
+						);
+
 					default:
 						return (
 							<ThemeText
 								key={index}
 								children={node.children.map((c, i) => renderNode(c, i))}
+								variant="textOrange"
 							/>
 						);
 				}
@@ -143,40 +193,25 @@ const HTMLParser = ({
 	};
 
 	return (
-		<ThemeText>
-			{!isFeedDetail && isTruncated
-				? `${unwrappedContent.slice(0, MAX_CHAR_COUNT)}...`
-				: renderContent()}
-			{isTruncated && !isFeedDetail && (
-				<ThemeText
-					variant="textOrange"
-					size="fs_13"
-					className="p-3"
-					onPress={handleSeeMorePress}
-				>
-					{'  '}
-					See More
-				</ThemeText>
-			)}
-		</ThemeText>
+		<>
+			<ThemeText children={document.children.map(renderNode)} />
+		</>
 	);
 };
 
 export default HTMLParser;
 
+// Utility function to unwrap content from child nodes
 const unwrapNode = (node: ChildNode): string => {
 	switch (node.type) {
 		case ElementType.Text:
 			return node.data;
 		case ElementType.Tag:
 			if (node.name === 'span') {
-				if (node.attribs.class?.includes('invisible')) {
-					return '';
-				}
+				if (node.attribs.class?.includes('invisible')) return '';
 				if (node.attribs.class?.includes('ellipsis'))
 					return `${node.children.map(child => unwrapNode(child)).join('')}...`;
 			}
-
 			return node.children.map(child => unwrapNode(child)).join('');
 		default:
 			return '';
