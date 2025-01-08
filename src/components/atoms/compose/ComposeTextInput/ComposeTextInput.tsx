60a0d5c96db4311e6f918544417532da2ec4771e
yemyatthu-binlab <yemyatthu.cs@gmail.com>
2025-01-08 23:10:22 +0630

    Fix:: paste operation not being able to do twice in android

--- src/components/atoms/compose/ComposeTextInput/ComposeTextInput.tsx
+++ src/components/atoms/compose/ComposeTextInput/ComposeTextInput.tsx
@@ -1,12 +1,15 @@
-import { TextInput, TextInputProps } from 'react-native';
+import { TextInput, TextInputProps, View } from 'react-native';
 import useAppropiateColorHash from '@/hooks/custom/useAppropiateColorHash';
 import { useComposeStatus } from '@/context/composeStatusContext/composeStatus.context';
 import { FormattedText } from '../FormattedText/FormattedText';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback, useRef } from 'react';
 
 const ComposeTextInput = ({ ...textInputProps }: TextInputProps) => {
 	const inputColor = useAppropiateColorHash('patchwork-light-900');
 	const selectionColor = useAppropiateColorHash('patchwork-red-50');
 	const { composeState, composeDispatch } = useComposeStatus();
+	const inputRef = useRef<TextInput>(null);
 
 	const handleChangeText = (text: string) => {
 		composeDispatch({
@@ -22,17 +25,23 @@ const ComposeTextInput = ({ ...textInputProps }: TextInputProps) => {
 		}
 	};
 
+	useFocusEffect(
+		useCallback(() => {
+			// Focus the input when the screen is focused
+			inputRef.current?.focus();
+			inputRef.current?.clear();
+		}, []),
+	);
+
 	return (
 		<TextInput
 			placeholder="Start Typing here.."
+			ref={inputRef}
 			multiline
 			maxLength={composeState.maxCount}
 			placeholderTextColor={inputColor}
-			scrollEnabled={false}
 			selectionColor={selectionColor}
 			onChangeText={handleChangeText}
-			autoCorrect
-			autoComplete="off"
 			autoFocus
 			autoCapitalize="none"
 			spellCheck
